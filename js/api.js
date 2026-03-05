ded466641d347c818f10c3fa80c758266d1a9f17
Note-salaf <123761829+Note-salaf@users.noreply.github.com>
2026-03-05 06:29:10 -0300

    Update api.js
    fix: prevent API caching by adding timestamp to request URL

--- js/api.js
+++ js/api.js
@@ -1,7 +1,8 @@
 export async function buscarConselho() {
-    const resposta = await fetch('https://api.adviceslip.com/advice', {
+    const resposta = await fetch('https://api.adviceslip.com/advice?${Date.now()}`', {
         cache: "no-store"})
     const dados = await resposta.json()
     return dados.slip.advice
 
 }
+
